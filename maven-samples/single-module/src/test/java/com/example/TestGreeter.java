7d80e90b8f5bec0234f16cd4621bc40194441664
github <github703@gmail.com>
2024-12-19 20:30:44 +0530

    second polling job

--- maven-samples/single-module/src/test/java/com/example/TestGreeter.java
+++ maven-samples/single-module/src/test/java/com/example/TestGreeter.java
@@ -37,7 +37,7 @@ public class TestGreeter {
 
     @Test
   public void greetShouldIncludeGreetingMessage() {
-    String someone = "World,Thanks for devops calss test";
+    String someone = "World,Thanks for devops training";
 
     assertThat(greeter.greet(someone).length(), is(greaterThan(someone.length())));
   }
